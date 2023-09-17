d9dece3c308d3e0da4e17674fa646d87179eba1e
PavelGN <142707492+PavelGN@users.noreply.github.com>
2023-09-17 10:57:09 +0600

    Add files via upload

---
+++ cypress/e2e/Pokemons.cy.js
@@ -0,0 +1,57 @@
+describe('Тестируем форму логина', function () {
+   it('Верный логин и верный пароль', function () {
+        cy.visit('https://pokemonbattle.me/login');
+        cy.get(':nth-child(1) > .auth__input').type('pavel_wower@mail.ru');
+        cy.get('#password').type('Xzxzzxzxzca1988');
+        cy.get('.auth__button').click();
+        cy.get('.header__btns > [href="/shop"]').click();
+        const randomIndex = Math.floor(Math.random() * 12) + 1;
+        cy.get(`:nth-child(${randomIndex}) > .shop__button`).click();
+        cy.get('.pay__select-block').should('exist').should('be.visible');
+        cy.get('.payment__info-text-new').should('exist').should('be.visible');
+        cy.get('.payment__info-button-v2').should('exist').should('be.visible');
+        cy.get('.payment__info-button-v2').click();
+        cy.get('.payment__subblock').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(1)').should('exist').should('be.visible');
+        cy.get('.payment__subblock > .payment__shopname').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(3)').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(4)').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(5)').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(6)').should('exist').should('be.visible');
+        cy.get('.pay__payform-v2').should('exist').should('be.visible');
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4111111111111111').should('exist').should('be.visible');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1224').should('exist').should('be.visible');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125').should('exist').should('be.visible');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('Fedor Ivanov').should('exist').should('be.visible');
+        cy.get('.pay-btn').should('be.enabled');
+        cy.get('.pay-btn').click();
+        cy.get('.pay__select-block').should('exist').should('be.visible');
+        cy.get('.payment__info-text-new').should('exist').should('be.visible');
+        cy.get('.payment__info-button-v2').should('exist').should('be.visible');
+        cy.get('.payment__info-button-v2').click();
+        cy.get('.payment__subblock').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(1)').should('exist').should('be.visible');
+        cy.get('.payment__subblock > .payment__shopname').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(3)').should('exist').should('be.visible');
+        cy.get('.payment__span_number').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(5)').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(6)').should('exist').should('be.visible');
+        cy.get('.payment__form-defolt').should('exist').should('be.visible');
+        cy.get('#cardnumber').type('56456').should('exist').should('be.visible');
+        cy.get('.payment__span-defolt').should('exist').should('be.visible');
+        cy.get('.payment__defolt-decline').should('exist').should('be.visible');
+        cy.get('.payment__submit-button').should('be.enabled');
+        cy.get('.payment__submit-button').click();
+        cy.get('.payment__adv').should('exist').should('be.visible');
+        cy.contains('Покупка прошла успешно').should('exist').should('be.visible');
+        cy.get('.payment__info-button-v2').click();
+        cy.get('.payment__subblock').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(1)').should('exist').should('be.visible');
+        cy.get('.payment__subblock > .payment__shopname').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(3)').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(4)').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(5)').should('exist').should('be.visible');
+        cy.get('.payment__subblock > :nth-child(6)').should('exist').should('be.visible');
+        cy.get('.payment__adv').should('exist').should('be.visible');
+           })
+})
